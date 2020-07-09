09b87f82ea522b546c1ec7774da5cad9232d5e04
hbinn <50905732+hbinn@users.noreply.github.com>
2020-07-09 21:43:54 +0900

    Add files via upload

---
+++ hyebin/week_1/hash3.py
@@ -0,0 +1,13 @@
+def solution(clothes):
+    check = {}
+    sum = 1
+    for i in clothes:
+        check[i[1]] = 1
+
+    for i in clothes:
+        check[i[1]] += 1
+
+    for i in check :
+        sum = sum * check[i]
+    answer = sum - 1
+    return answer
