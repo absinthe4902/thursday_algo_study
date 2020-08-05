25c781ec5a9affc4385c5ef1d2b23978e3ec6652
Seungmin Lee <seungmin.lee@dnx.kr>
2020-08-05 17:45:39 +0900

    ADD:merge

--- seungmin/week_3/queue4.py
+++ seungmin/week_3/queue4.py
@@ -1,4 +1,31 @@
-def solution(prices):
-    answer = []
-    
+def solution(arrangement):
+    arr = list(arrangement.replace('()', '1', arrangement.count('()')))
+    stack = []
+    answer = 0
+    index = 0
+    cutter = 0
+    checker = 0
+    length = len(arr)
+    while index < length:
+        temp = arr[index]
+
+        if temp == ')':
+            for i in range(index-1, -1, -1):
+                if arr[i] == '1':
+                    cutter = cutter + 1
+                elif arr[i] == '(':
+                    arr[i] = '0'
+                    answer = answer + cutter + 1
+                    cutter = 0
+                    break
+        index = index + 1
     return answer
+
+
+
+cd
+
+
+
+print(solution('()(((()())(())()))(())'))
+
